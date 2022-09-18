290e5a40ac9bb2c84911a137f97b92672985282c
Fernando Silvano Gil Pérez <fsgil@indra.es>
2022-09-18 15:27:59 +0200

    Configuración bbdd en la nube

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/thot"
+  //url: "mongodb://localhost:27017/thot"
+  url: "mongodb+srv://angelheroku:angelmunoz23@thot.4vi0mon.mongodb.net/thot?retryWrites=true&w=majority"
 };
